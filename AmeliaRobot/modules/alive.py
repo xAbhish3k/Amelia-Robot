ea68b9e685cd796f7ee42f553283918967828af6
Abhishek singh <84198635+xAbhish3k@users.noreply.github.com>
2022-07-02 22:45:27 +0530

    Create alive.py

---
+++ AmeliaRobot/modules/alive.py
@@ -0,0 +1,36 @@
+import os
+import re
+import random
+from platform import python_version as kontol
+from telethon import events, Button
+from telegram import __version__ as telever
+from telethon import __version__ as tlhver
+from pyrogram import __version__ as pyrover
+from FallenRobot.events import register
+from FallenRobot import telethn as tbot
+
+
+PHOTO = [
+    "https://telegra.ph/file/6a21ea6677342f43b363e.jpg",
+    "https://telegra.ph/file/40eb1ed850cdea274693e.jpg",
+]
+
+
+@register(pattern=("/alive"))
+async def awake(event):
+    TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nAmelia ʀᴏʙᴏᴛ​**\n━━━━━━━━━━━━━━━━━━━\n\n"
+    TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [abhishek](https://t.me/xabhish3k)** \n\n"
+    TEXT += f"» **ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{telever}` \n\n"
+    TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{tlhver}` \n\n"
+    TEXT += f"» **ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{pyrover}` \n━━━━━━━━━━━━━━━━━\n\n"
+    BUTTON = [
+        [
+            Button.url("ʜᴇʟᴘ​", "https://t.me/ameliagroup_bot?start=help"),
+            Button.url("sᴜᴘᴘᴏʀᴛ​", "https://t.me/warbotzsupport"),
+        ]
+    ]
+    ran = random.choice(PHOTO)
+    await tbot.send_file(event.chat_id, ran, caption=TEXT, buttons=BUTTON)
+
+
+## Alive mod

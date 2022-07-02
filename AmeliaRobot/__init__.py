33dd1a00c61af11ed2baf3fd2f62280adc44bc03
Abhishek singh <84198635+xAbhish3k@users.noreply.github.com>
2022-07-02 22:08:51 +0530

    Update __init__.py

--- AmeliaRobot/__init__.py
+++ AmeliaRobot/__init__.py
@@ -102,7 +102,7 @@ if ENV:
         raise Exception("Your blacklisted chats list does not contain valid integers.")
 
 else:
-    from FallenRobot.config import Development as Config
+    from AmeliaRobot.config import Development as Config
 
     TOKEN = Config.TOKEN
 
@@ -196,7 +196,7 @@ DEMONS = list(DEMONS)
 TIGERS = list(TIGERS)
 
 # Load at end to ensure all prev variables have been set
-from FallenRobot.modules.helper_funcs.handlers import (
+from AmeliaRobot.modules.helper_funcs.handlers import (
     CustomCommandHandler,
     CustomMessageHandler,
     CustomRegexHandler,

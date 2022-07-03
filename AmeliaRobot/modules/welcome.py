78c9eebeb30e73742597c8d7656e482bf1f45eae
Abhishek singh <84198635+xAbhish3k@users.noreply.github.com>
2022-07-03 11:28:01 +0530

    Update welcome.py

--- AmeliaRobot/modules/welcome.py
+++ AmeliaRobot/modules/welcome.py
@@ -259,7 +259,7 @@ def new_member(update: Update, context: CallbackContext):
             # Welcome yourself
             elif new_mem.id == bot.id:
                 creator = None
-                if not FallenRobot.ALLOW_CHATS:
+                if not AmeliaRobot.ALLOW_CHATS:
                     with suppress(BadRequest):
                         update.effective_message.reply_text(
                             f"Groups are disabled for {bot.first_name}, I'm outta here."

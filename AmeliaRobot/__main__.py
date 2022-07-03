1efef197678890fdb2ac6d51f66b536a6dee5bdf
Abhishek singh <84198635+xAbhish3k@users.noreply.github.com>
2022-07-03 11:21:07 +0530

    Update __main__.py

--- AmeliaRobot/__main__.py
+++ AmeliaRobot/__main__.py
@@ -754,7 +754,7 @@ def main():
     settings_callback_handler = CallbackQueryHandler(settings_button, pattern=r"stngs_")
 
     about_callback_handler = CallbackQueryHandler(
-        Fallen_about_callback, pattern=r"fallen_"
+        Amelia_about_callback, pattern=r"amelia_"
     )
     source_callback_handler = CallbackQueryHandler(
         Source_about_callback, pattern=r"source_"
